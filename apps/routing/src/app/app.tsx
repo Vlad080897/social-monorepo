71b7e438b359b35bc79b9c9f3c1f407d4838dfea
vlad.vitkovskyi <vvitkovskij4@gmail.com>
2025-06-10 17:40:09 +0300

    feat: add base packages

---
+++ apps/routing/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@react-monorepo/routing" />
+    </div>
+  );
+}
+
+export default App;
